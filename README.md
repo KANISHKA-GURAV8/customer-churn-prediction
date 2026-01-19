6988707b575e672d64a10b7ff771638dd70fbf75
KANISHKA-GURAV2005 <12345678+username@users.noreply.github.com>
2026-01-19 20:55:58 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Customer Churn Prediction Using Machine Learning
+
+This project predicts whether a customer is likely to churn using machine learning techniques.
+The model is trained on telecom customer data and deployed using a Flask web application.
+
+## Features
+- Supervised ML classification model
+- Handles class imbalance using SMOTE
+- Predicts churn and churn probability
+- Web interface using Flask and HTML
+
+## Technologies Used
+- Python
+- Pandas, NumPy
+- Scikit-learn
+- Imbalanced-learn
+- Flask
+- HTML, CSS
+
+## How to Run the Project
+1. Clone the repository
+2. Install dependencies:
+pip install -r requirements.txt
+
+3. Run the Flask app:
+python app.py
+
+4. Open browser and go to:
+http://127.0.0.1:5000/
+
+
+## Output
+The application predicts whether a customer will churn and displays the churn probability.
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request
+import pandas as pd
+import joblib
+
+app=Flask(__name__)
+model=joblib.load("best_churn_model.joblib")
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    data={
+        "AccountWeeks":float(request.form["AccountWeeks"]),
+        "ContractRenewal":float(request.form["ContractRenewal"]),
+        "DataPlan":float(request.form["DataPlan"]),
+        "DataUsage":float(request.form["DataUsage"]),
+        "CustServCalls":int(request.form["CustServCalls"]),
+        "DayMins":float(request.form["DayMins"]),
+        "DayCalls":int(request.form["DayCalls"]),
+        "MonthlyCharge":float(request.form["MonthlyCharge"]),
+        "OverageFee":float(request.form["OverageFee"]),
+        "RoamMins":float(request.form["RoamMins"])
+
+
+    }
+
+    input_df=pd.DataFrame([data])
+    prediction=model.predict(input_df)[0]
+    probability=model.predict_proba(input_df)[0][1]
+    result="Customer will churn" if prediction==1 else "customer will not churn"
+
+    return render_template(
+        "result.html",
+        result=result,
+        probability=f"{probability*100:2f}%"
+    )
+
+if __name__=="__main__":
+    app.run(debug=True)
